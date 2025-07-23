ca1714824931c32f74db78f7cc34d39d598b0d92
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-23 03:00:39 +0200

    Update tab5_camera.h

--- components/tab5_camera/tab5_camera.h
+++ components/tab5_camera/tab5_camera.h
@@ -5,13 +5,20 @@
 
 #ifdef USE_ESP32
 
+// Vérification de disponibilité d'esp_camera
+#if __has_include("esp_camera.h")
 #include "esp_camera.h"
 #include "esp_system.h"
 #include "esp_log.h"
+#define HAS_ESP_CAMERA
+#else
+#warning "esp_camera.h not found - Tab5 Camera component will be disabled"
+#endif
 
 namespace esphome {
 namespace tab5_camera {
 
+#ifdef HAS_ESP_CAMERA
 class Tab5Camera : public Component {
  public:
   void setup() override;
@@ -37,9 +44,23 @@ class Tab5Camera : public Component {
   camera_config_t camera_config_;
   bool camera_initialized_{false};
 };
+#else
+class Tab5Camera : public Component {
+ public:
+  void setup() override { ESP_LOGE("tab5_camera", "esp_camera.h not available"); }
+  void dump_config() override {}
+  float get_setup_priority() const override { return 0; }
+  void set_name(const std::string &name) {}
+  void set_external_clock_pin(uint8_t pin) {}
+  void set_external_clock_frequency(uint32_t frequency) {}
+  void set_reset_pin(GPIOPin *pin) {}
+  bool take_snapshot() { return false; }
+};
+#endif
 
 }  // namespace tab5_camera
 }  // namespace esphome
 
 #endif  // USE_ESP32
+
 
