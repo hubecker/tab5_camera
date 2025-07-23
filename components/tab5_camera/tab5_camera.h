3593d9676be5e38ab510d109575c12bd8cba788d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-23 02:50:26 +0200

    Update tab5_camera.h

--- components/tab5_camera/tab5_camera.h
+++ components/tab5_camera/tab5_camera.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/components/camera/camera.h"
 
 #ifdef USE_ESP32
 
@@ -13,22 +12,24 @@
 namespace esphome {
 namespace tab5_camera {
 
-class Tab5Camera : public camera::Camera, public Component {
+class Tab5Camera : public Component {
  public:
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
   
+  void set_name(const std::string &name) { this->name_ = name; }
   void set_external_clock_pin(uint8_t pin) { this->external_clock_pin_ = pin; }
   void set_external_clock_frequency(uint32_t frequency) { this->external_clock_frequency_ = frequency; }
   void set_reset_pin(GPIOPin *pin) { this->reset_pin_ = pin; }
   
-  camera::CameraImage *snapshot() override;
+  bool take_snapshot();
 
  protected:
   bool init_camera_();
   void deinit_camera_();
   
+  std::string name_;
   uint8_t external_clock_pin_{36};
   uint32_t external_clock_frequency_{20000000}; // 20MHz
   GPIOPin *reset_pin_{nullptr};
@@ -41,3 +42,4 @@ class Tab5Camera : public camera::Camera, public Component {
 }  // namespace esphome
 
 #endif  // USE_ESP32
+
