922500178191dc8f01854a94e335b1cd6d2ce6dc
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-23 18:26:21 +0200

    Update __init__.py

--- components/tab5_camera/__init__.py
+++ components/tab5_camera/__init__.py
@@ -7,11 +7,10 @@ from esphome import pins
 DEPENDENCIES = ['esp32', 'i2c']
 
 tab5_camera_ns = cg.esphome_ns.namespace('tab5_camera')
-Tab5Camera = tab5_camera_ns.class_('Tab5Camera', cg.Component)
+Tab5Camera = tab5_camera_ns.class_('Tab5Camera', cg.Component, i2c.I2CDevice)
 
 CONF_EXTERNAL_CLOCK = "external_clock"
 CONF_RESET_PIN = "reset_pin"
-CONF_I2C_ID = "i2c_id"
 CONF_SENSOR_ADDRESS = "sensor_address"
 
 # Schéma pour external_clock
@@ -25,32 +24,32 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_NAME, default="Tab5 Camera"): cv.string,
     cv.Optional(CONF_EXTERNAL_CLOCK): EXTERNAL_CLOCK_SCHEMA,
     cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_I2C_ID): cv.use_id(i2c.I2CComponent),
     cv.Optional(CONF_SENSOR_ADDRESS, default=0x24): cv.i2c_address,
-}).extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(default_address=0x24))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
     
     # Configuration du nom
     cg.add(var.set_name(config[CONF_NAME]))
     
-    # Configuration I2C
-    if CONF_I2C_ID in config:
-        i2c_component = await cg.get_variable(config[CONF_I2C_ID])
-        cg.add(var.set_i2c_parent(i2c_component))
-    
     # Configuration de l'adresse du capteur
-    cg.add(var.set_sensor_address(config[CONF_SENSOR_ADDRESS]))
+    if CONF_SENSOR_ADDRESS in config:
+        cg.add(var.set_sensor_address(config[CONF_SENSOR_ADDRESS]))
     
     # Configuration de l'horloge externe
     if CONF_EXTERNAL_CLOCK in config:
         clock_config = config[CONF_EXTERNAL_CLOCK]
         
-        # Pin de l'horloge
-        pin = await gpio.gpio_pin_expression(clock_config[CONF_PIN])
-        cg.add(var.set_external_clock_pin(pin))
+        # Pin de l'horloge - correction pour récupérer le numéro de GPIO
+        pin_config = clock_config[CONF_PIN]
+        if isinstance(pin_config, dict):
+            pin_num = pin_config.get('number', pin_config.get('pin', 0))
+        else:
+            pin_num = int(pin_config)
+        cg.add(var.set_external_clock_pin(pin_num))
         
         # Fréquence de l'horloge (convertir en Hz)
         freq = clock_config[CONF_FREQUENCY]
@@ -64,6 +63,7 @@ async def to_code(config):
     # Ajout des flags de compilation nécessaires
     cg.add_build_flag("-DHAS_ESP32_P4_CAMERA=1")
     cg.add_build_flag("-DCONFIG_IDF_TARGET_ESP32P4=1")
+
 
 
 
