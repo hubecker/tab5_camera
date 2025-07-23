766c63951dbb3d436351ae33b963508dae36a3a8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-23 02:50:04 +0200

    Update __init__.py

--- components/tab5_camera/__init__.py
+++ components/tab5_camera/__init__.py
@@ -2,27 +2,29 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import camera
 from esphome.const import CONF_ID, CONF_NAME
+from esphome.core import CORE
 
 DEPENDENCIES = ["esp32"]
-CODEOWNERS = ["@ton-username"]
+CODEOWNERS = ["@youkorr"]
 
 tab5_camera_ns = cg.esphome_ns.namespace("tab5_camera")
-Tab5Camera = tab5_camera_ns.class_("Tab5Camera", camera.Camera, cg.Component)
+Tab5Camera = tab5_camera_ns.class_("Tab5Camera", cg.Component)
 
-CONFIG_SCHEMA = camera.CAMERA_SCHEMA.extend(
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Tab5Camera),
+        cv.Required(CONF_NAME): cv.string,
         cv.Optional("external_clock_pin", default=36): cv.int_,
-        cv.Optional("external_clock_frequency", default="20MHz"): cv.frequency,
+        cv.Optional("external_clock_frequency", default=20000000): cv.int_,
         cv.Optional("reset_pin"): cv.pin_schema,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await camera.register_camera(var, config)
     await cg.register_component(var, config)
     
+    cg.add(var.set_name(config[CONF_NAME]))
     cg.add(var.set_external_clock_pin(config["external_clock_pin"]))
     cg.add(var.set_external_clock_frequency(config["external_clock_frequency"]))
     
