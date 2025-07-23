69e64a7a3e624c0f8f0ff9bc3a1445a1d994b097
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-23 03:03:41 +0200

    Update tab5_camera.h

--- components/tab5_camera/tab5_camera.h
+++ components/tab5_camera/tab5_camera.h
@@ -11,14 +11,11 @@
 #include "esp_system.h"
 #include "esp_log.h"
 #define HAS_ESP_CAMERA
-#else
-#warning "esp_camera.h not found - Tab5 Camera component will be disabled"
 #endif
 
 namespace esphome {
 namespace tab5_camera {
 
-#ifdef HAS_ESP_CAMERA
 class Tab5Camera : public Component {
  public:
   void setup() override;
@@ -33,34 +30,24 @@ class Tab5Camera : public Component {
   bool take_snapshot();
 
  protected:
+#ifdef HAS_ESP_CAMERA
   bool init_camera_();
   void deinit_camera_();
+  
+  camera_config_t camera_config_;
+  bool camera_initialized_{false};
+#endif
   
   std::string name_;
   uint8_t external_clock_pin_{36};
   uint32_t external_clock_frequency_{20000000}; // 20MHz
   GPIOPin *reset_pin_{nullptr};
-  
-  camera_config_t camera_config_;
-  bool camera_initialized_{false};
 };
-#else
-class Tab5Camera : public Component {
- public:
-  void setup() override { ESP_LOGE("tab5_camera", "esp_camera.h not available"); }
-  void dump_config() override {}
-  float get_setup_priority() const override { return 0; }
-  void set_name(const std::string &name) {}
-  void set_external_clock_pin(uint8_t pin) {}
-  void set_external_clock_frequency(uint32_t frequency) {}
-  void set_reset_pin(GPIOPin *pin) {}
-  bool take_snapshot() { return false; }
-};
-#endif
 
 }  // namespace tab5_camera
 }  // namespace esphome
 
 #endif  // USE_ESP32
+
 
 
