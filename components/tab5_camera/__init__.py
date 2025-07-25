6245a381cc8b2a0994cf0f336b6f5ac8d4caf372
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-25 19:58:18 +0200

    Update __init__.py

--- components/tab5_camera/__init__.py
+++ components/tab5_camera/__init__.py
@@ -55,7 +55,7 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_EXTERNAL_CLOCK_PIN, default=0): cv.int_range(min=0, max=255),
             cv.Optional(CONF_FREQUENCY, default=20000000): cv.positive_int,
             cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_SENSOR_ADDRESS, default=0x3C): cv.i2c_address,
+            cv.Optional(CONF_SENSOR_ADDRESS, default=0x3D): cv.i2c_address,
             # Nouveaux paramètres
             cv.Optional(CONF_RESOLUTION, default="VGA"): validate_resolution,
             cv.Optional(CONF_PIXEL_FORMAT, default="YUV422"): cv.one_of(*PIXEL_FORMATS.keys(), upper=True),
