81aabb74c543bde0f642803ccc43262977e2a586
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-25 05:52:35 +0200

    Update tab5_camera.cpp

--- components/tab5_camera/tab5_camera.cpp
+++ components/tab5_camera/tab5_camera.cpp
@@ -68,6 +68,21 @@ void Tab5Camera::setup() {
   this->mark_failed();
 #endif
 }
+void Tab5Camera::streaming_task() {
+  this->log("Streaming loop started for camera '%s'", this->name_.c_str());
+
+  while (this->is_streaming_) {
+    if (xSemaphoreTake(this->frame_ready_semaphore_, pdMS_TO_TICKS(1000))) {
+
+      esp_cam_ctlr_receive(this->cam_handle_, &this->current_frame_, portMAX_DELAY);
+    } else {
+      ESP_LOGW(TAG, "No frame received in time");
+    }
+  }
+
+  this->log("Streaming task exiting");
+  vTaskDelete(nullptr);
+}
 
 void Tab5Camera::dump_config() {
   ESP_LOGCONFIG(TAG, "Tab5 Camera '%s':", this->name_.c_str());
