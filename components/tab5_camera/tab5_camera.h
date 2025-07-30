311fd15e79b53c0dc0787450d9805eb7d4e3f56e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-30 05:43:23 +0200

    Update tab5_camera.h

--- components/tab5_camera/tab5_camera.h
+++ components/tab5_camera/tab5_camera.h
@@ -199,7 +199,7 @@ class Tab5Camera : public Component, public i2c::I2CDevice {
   std::string name_{"Tab5 Camera"};
   uint8_t external_clock_pin_{0};
   uint32_t external_clock_frequency_{24000000};  // 24MHz par défaut
-  uint8_t sensor_address_{0x44};  // Adresse I2C par défaut du capteur SC2356
+  uint8_t sensor_address_{0x24};  // Adresse I2C par défaut du capteur SC2356
   GPIOPin *reset_pin_{nullptr};
 
   // Paramètres de caméra
