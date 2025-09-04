5ee5f2c12d98a15a9be24e1fa1f53eb211470957
youkorr <96953496+youkorr@users.noreply.github.com>
2025-09-04 12:43:34 +0200

    Update tab5_camera.cpp

--- components/tab5_camera/tab5_camera.cpp
+++ components/tab5_camera/tab5_camera.cpp
@@ -4,6 +4,7 @@
 #include "esphome/core/hal.h"
 #include "esp_timer.h"
 #include "esp_cam_ctlr_csi.h"
+#include "sdkconfig.h"
 //#include "esp_cam_sensor.h"
 #include "driver/jpeg_encode.h"
 //#include "esp_video_buffer.h"
